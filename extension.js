ad77ab3598eff928e0c030eb468d1dc1fed44ce2
Matt Vogel <mainmoniker@googlemail.com>
2022-07-26 07:42:32 -0400

    fix: remove illegal char in setting names

--- extension.js
+++ extension.js
@@ -1,10 +1,10 @@
 /* Original code by matt vogel */
   /* v1  */
-let sidebarBtnClasses = ['.bp3-icon-calendar', '.bp3-icon-graph', '.bp3-icon-list', '.bp3-icon-shop']
+let sidebarBtnClasses = ['bp3-icon-calendar', 'bp3-icon-graph', 'bp3-icon-list', 'bp3-icon-shop']
 
 // select parent button by child icon
 function selectButton(buttonClass){
-  let icon = document.querySelector(".roam-sidebar-content " + buttonClass);
+  let icon = document.querySelector(".roam-sidebar-content ." + buttonClass);
   let div = icon.parentNode;
   // some buttons (daily notes, graph) have a different html structure
   if (div.className != 'log-button'){
@@ -34,33 +34,33 @@ function toggleButton(checked, btnClass){
 const panelConfig = {
     tabTitle: "Hide Left Sidebar Buttons",
     settings: [
-        {id:         ".bp3-icon-calendar",
+        {id:         "bp3-icon-calendar",
         name:        "Hide Daily Notes Button",
         description: "Hides or shows the Graph Overview button in the left sidebar",
         action:      {type:     "switch",
                       onChange: (evt) => { 
-                        toggleButton(evt.target.checked, '.bp3-icon-calendar')
+                        toggleButton(evt.target.checked, 'bp3-icon-calendar')
                     }}},
-        {id:          ".bp3-icon-graph",
+        {id:          "bp3-icon-graph",
          name:        "Hide Graph Button",
          description: "Hides or shows the Graph Overview button in the left sidebar",
          action:      {type:     "switch",
                        onChange: (evt) => { 
-                        toggleButton(evt.target.checked, '.bp3-icon-graph')
+                        toggleButton(evt.target.checked, 'bp3-icon-graph')
                       }}},
-        {id:          ".bp3-icon-list",
+        {id:          "bp3-icon-list",
         name:        "Hide All Pages Button",
         description: "Hides or shows the Roam Depot button in the left sidebar",
         action:      {type:     "switch",
                       onChange: (evt) => { 
-                        toggleButton(evt.target.checked, '.bp3-icon-list')
+                        toggleButton(evt.target.checked, 'bp3-icon-list')
                       }}},
-        {id:          ".bp3-icon-shop",
+        {id:          "bp3-icon-shop",
         name:        "Hide Roam Depot Button",
         description: "Hides or shows the Roam Depot button in the left sidebar",
         action:      {type:     "switch",
                       onChange: (evt) => { 
-                        toggleButton(evt.target.checked, '.bp3-icon-shop')
+                        toggleButton(evt.target.checked, 'bp3-icon-shop')
                       }}}
     ]
 };
