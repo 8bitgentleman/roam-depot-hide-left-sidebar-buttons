8542b3d2b0446bbef29a56174ec41eac02836916
Matt Vogel <mainmoniker@googlemail.com>
2023-01-22 15:14:41 -0500

    feat: WIP hide only on mobile

--- extension.js
+++ extension.js
@@ -1,5 +1,6 @@
 /* Original code by matt vogel */
   /* v1  */
+let mobile = false;
 let sidebarBtnClasses = ['rm-left-sidebar__daily-notes',
                         'rm-left-sidebar__graph-overview',
                         'rm-left-sidebar__all-pages',
@@ -23,14 +24,76 @@ function toggleButton(checked, btnClass){
   } 
 }
 
-const panelConfig = {
+
+
+async function onload({extensionAPI}) {
+  // set default setting
+  if (window.roamAlphaAPI.platform.isMobileApp||window.roamAlphaAPI.platform.isMobile||window.roamAlphaAPI.platform.isTouchDevice||window.roamAlphaAPI.platform.isIOS){
+    mobile = true;
+  }else{
+    mobile = false
+  }
+
+  for (let i = 0; i < sidebarBtnClasses.length; i++) {
+
+    if (!extensionAPI.settings.get(sidebarBtnClasses[i])) {
+      await extensionAPI.settings.set(sidebarBtnClasses[i], false);
+    } else if (extensionAPI.settings.get(sidebarBtnClasses[i])==true){
+      // when starting up check if we should only hide on mobile
+      if (extensionAPI.settings.get("mobile")==true) {
+        if(mobile==true){
+          hideButton(sidebarBtnClasses[i])
+        }
+      } else{
+        hideButton(sidebarBtnClasses[i])
+      }
+    }
+  }
+
+  const panelConfig = {
     tabTitle: "Hide Left Sidebar Buttons",
     settings: [
+      {
+            id: "mobile",
+            name: "Platform Toggle",
+            description: "Turn on to only hide items on Mobile, leave off for all platforms",
+            action: {
+                type: "switch",
+                onChange: (evt) => { 
+                  if (evt.target.checked) {
+                    console.log("hide only on mobile")
+                    if (mobile) {
+                      sidebarBtnClasses.forEach(function(button) {
+                        // check the api for if we want to hide the button
+                        if (extensionAPI.settings.get(button)) {
+                          hideButton(button)
+                        }
+                      })
+                    } else {
+                      // we want to only hide on mobile but it's not mobile
+                      sidebarBtnClasses.forEach(function(button) {
+                        showButton(button)
+                      })
+                    }
+                  } else {
+                    console.log("hide everywhere")
+                    sidebarBtnClasses.forEach(function(button) {
+                      // check the api for if we want to hide the button
+                      if (extensionAPI.settings.get(button)) {
+                        hideButton(button)
+                      }
+                    })
+                  }
+                  
+                }
+            },
+        },
         {id:         "rm-left-sidebar__daily-notes",
         name:        "Hide Daily Notes Button",
         description: "Hides or shows the Graph Overview button in the left sidebar",
         action:      {type:     "switch",
                       onChange: (evt) => { 
+                        // check if we are hiding on mobile and if it's mobile
                         toggleButton(evt.target.checked, 'rm-left-sidebar__daily-notes')
                     }}},
         {id:          "rm-left-sidebar__graph-overview",
@@ -55,18 +118,7 @@ const panelConfig = {
                         toggleButton(evt.target.checked, 'rm-left-sidebar__roam-depot')
                       }}}
     ]
-};
-
-async function onload({extensionAPI}) {
-  // set default setting
-  for (let i = 0; i < sidebarBtnClasses.length; i++) {
-
-    if (!extensionAPI.settings.get(sidebarBtnClasses[i])) {
-      await extensionAPI.settings.set(sidebarBtnClasses[i], false);
-    } else if (extensionAPI.settings.get(sidebarBtnClasses[i])==true){
-      hideButton(sidebarBtnClasses[i])
-    }
-  }
+  };
 
   console.log("load hide left sidebar button plugin")
   extensionAPI.settings.panel.create(panelConfig);
